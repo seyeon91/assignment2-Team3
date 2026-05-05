a4017bfd35af1f46aa8ff47c862b502967ab0712
hojunssss18293 <hojunssss18293@gmail.com>
2026-05-05 16:47:06 +0900

    *

--- MyApp.java
+++ MyApp.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 /**
- * MyApp 객체들을 입력받고 누구에게 물어볼지 몇단을 물어볼지 입력받는 프로그램
+ * MyApp 객체들을 입력받고 누구에게 물어볼지, 몇단을 물어볼지 입력받는 프로그램
  *
- * @author (2023320022 편규빈)
+ * @author (2023320022 편규빈, 2023320004 호준수)
  * @version (2026. 05. 05)
  */
 public class MyApp
@@ -24,5 +24,24 @@ public class MyApp
        System.out.print("원하는 구구단 수를 입력하세요: ");
        int dan = scanner.nextInt();
        
+       
+       if(dan >= 2 && dan <=9){
+           switch(targetName.toLowerCase()){
+               case "jenna":
+                   Jenna.Gugudan(dan);
+                   break;
+               case "john":
+                   John.Gugudan(dan);
+                   break;               
+               case "maria":
+                   Maria.Gugudan(dan);
+                   break;
+               case "james":
+                   James.Gugudan(dan);
+                   break;
+               default:
+                   System.out.println("해당 이름의 팀원이 없습니다.");
+           }
+       }
     }
 }
